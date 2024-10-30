3c2a048e4c168964f4959f8c4f2bfdfe547a387e
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-10-30 18:32:40 +0000

    Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -3,6 +3,11 @@ from anvil import *
 import anvil.server
 from anvil_extras import routing
 
+default_prompt = """
+This is a meeting transcript. Extract all the action items in a markdown list.
+Task Name
+
+"""
 
 @routing.route('', template='Router')
 @routing.route('/home', template='Router')
@@ -12,3 +17,6 @@ class Home(HomeTemplate):
         self.init_components(**properties)
         # anvil.server.call('do_admin')
         # Any code you write here will run before the form opens.
+        self.ta_prompt.text = default_prompt
+
+
