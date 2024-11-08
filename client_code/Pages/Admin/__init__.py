e4d5a9470e5a033c774f81e3b34f575ec4c61a3c
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-11-08 15:21:30 +0000

    Edited Pages.Admin

--- client_code/Pages/Admin/__init__.py
+++ client_code/Pages/Admin/__init__.py
@@ -1,5 +1,7 @@
 from ._anvil_designer import AdminTemplate
 from anvil import *
+import anvil.server
+from ...Global import Global
 
 
 class Admin(AdminTemplate):
@@ -7,8 +9,17 @@ class Admin(AdminTemplate):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
 
-        # Any code you write here will run before the form opens.
-
+    def form_show(self, **event_args):
+        """This method is called when the form is shown on the page"""
+        self.notion_stuff = Global.tenant_notion_info
+    
     def btn_save_notion_click(self, **event_args):
         """This method is called when the button is clicked"""
-        pass
+        anvil.server.call(
+            'save_tenant_notion',
+            self.tb_notion_api_key.text,
+            self.tb_notion_db_id.text,
+            self.tb_notion_userid.text
+        )
+
+
