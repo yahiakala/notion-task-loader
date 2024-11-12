88d1d97ddc28edc2074eaa475a73c4c8502c510f
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-11-12 22:43:48 +0000

    Edited Pages.Admin

--- client_code/Pages/Admin/__init__.py
+++ client_code/Pages/Admin/__init__.py
@@ -12,14 +12,15 @@ class Admin(AdminTemplate):
     def form_show(self, **event_args):
         """This method is called when the form is shown on the page"""
         self.notion_stuff = Global.tenant_notion_info
+        self.tb_notion_api_key.text = self.notion_stuff['notion_api_key']
+        self.tb_notion_db_id.text = self.notion_stuff['notion_db_id']
     
     def btn_save_notion_click(self, **event_args):
         """This method is called when the button is clicked"""
         anvil.server.call(
             'save_tenant_notion',
             self.tb_notion_api_key.text,
-            self.tb_notion_db_id.text,
-            self.tb_notion_userid.text
+            self.tb_notion_db_id.text
         )
 
 
