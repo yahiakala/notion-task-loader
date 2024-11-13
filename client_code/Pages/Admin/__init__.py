0336c1cb6df43188c238536ec544f930b83a865b
yahiakala <ymzkala@gmail.com>
2024-11-13 13:57:32 -0500

    added skeleton load

--- client_code/Pages/Admin/__init__.py
+++ client_code/Pages/Admin/__init__.py
@@ -11,9 +11,20 @@ class Admin(AdminTemplate):
 
     def form_show(self, **event_args):
         """This method is called when the form is shown on the page"""
+        with anvil.server.no_loading_indicator:
+            self.load_data()
+    
+    def load_data(self):
+        """Load data and remove skeleton states"""
         self.notion_stuff = Global.tenant_notion_info
+        
+        # Populate fields
         self.tb_notion_api_key.text = self.notion_stuff['notion_api_key']
         self.tb_notion_db_id.text = self.notion_stuff['notion_db_id']
+        
+        # Remove skeleton states
+        self.tb_notion_api_key.role = 'task-input'
+        self.tb_notion_db_id.role = 'task-input'
     
     def btn_save_notion_click(self, **event_args):
         """This method is called when the button is clicked"""
@@ -23,5 +34,3 @@ class Admin(AdminTemplate):
             self.tb_notion_api_key.text,
             self.tb_notion_db_id.text
         )
-
-
