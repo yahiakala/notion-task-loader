76300645ef7cee20c8c4cc41aff0e80b98924084
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-11-13 19:14:39 +0000

    Edited Global

--- client_code/Global.py
+++ client_code/Global.py
@@ -5,13 +5,13 @@ _global_dict = {
     'user': None,
     'is_mobile': None,
     'customer_portal': None,
-    'usertenant': None,
     'tenant': None,
     'tenant_id': None
 }
 _tenanted_dict = {
     'info': None,
     'permissions': None,
+    'usertenant': None,
     'tenant_notion_info': None
 }
 
