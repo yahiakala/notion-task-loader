b3ca295e8ed92c0508e8dc55696af0a1081bc459
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-11-15 02:30:32 +0000

    Edited routes

--- client_code/routes.py
+++ client_code/routes.py
@@ -10,15 +10,16 @@ class EnsureUserMixin:
         if not Global.user:
             raise Redirect(path="/signin")
         if Global.user and Global.get_s('tenant') is None:
-            Global.tenant = anvil.server.call('get_tenant_single')
-            if Global.get_s('tenant') is None:
-                Global.tenant = anvil.server.call('create_tenant_single')
-            try:
-                Global.tenant_id = Global.tenant.get_id()
-            except Exception:
-                Global.tenant_id = Global.tenant['id']
-            if 'delete_members' in Global.permissions and (Global.tenant['name'] is None or Global.tenant['name'] == ''):
-                raise Redirect(path='/app/settings')
+            with anvil.server.no_loading_indicator:
+                Global.tenant = anvil.server.call('get_tenant_single')
+                if Global.get_s('tenant') is None:
+                    Global.tenant = anvil.server.call('create_tenant_single')
+                try:
+                    Global.tenant_id = Global.tenant.get_id()
+                except Exception:
+                    Global.tenant_id = Global.tenant['id']
+                if 'delete_members' in Global.permissions and (Global.tenant['name'] is None or Global.tenant['name'] == ''):
+                    raise Redirect(path='/app/settings')
 
 
 class SignRoute(BaseRoute):
